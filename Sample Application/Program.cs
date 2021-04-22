a511ebce997f6f64fe93001f405078542be97c65
unknown <adilamc.mec@gmail.com>
2021-04-22 11:24:21 +0530

    first commit

---
+++ Sample Application/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6, c = 7, d = 7;
+            int b = 8;
+            Console.WriteLine("sum of"+a+"+"+b+"="+(a + b));
+            Console.WriteLine("Diff of"+a+"-"+b+"="+(a - b));
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.ReadLine();
+            //Sum
+            //Minus
+            //Multiply
+            //equality
+            //Divide
+        }
+    }
+}
